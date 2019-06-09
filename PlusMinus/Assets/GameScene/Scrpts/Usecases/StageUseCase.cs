fa5de7d8cc2ec3f754d504e2b7867ca9c314da8b
mastermonk <mastermonk@studioclips.sakura.ne.jp>
2019-06-09 16:50:57 +0900

    PlusMinus の初期プログラム

---
+++ PlusMinus/Assets/GameScene/Scrpts/Usecases/StageUseCase.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * カメラの距離として
+ * ６ｘ６のときは距離を１２に、５ｘ５のときは１０に、４ｘ４のときは８に、３ｘ３のときは６に設定
+ * */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageUseCase : MonoBehaviour
+{
+    [SerializeField]
+    private Camera 
+    // Start is called before the first frame update
+    void Start()
+    {
+        //  4x4として一旦設定
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
